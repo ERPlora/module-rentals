59937aa69123cd9897d1641336e96921ca151ac6
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:11 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,30 +1,30 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'rentals'
-    MODULE_NAME = _('Rental Management')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'key-outline'
-    MODULE_DESCRIPTION = _('Equipment, vehicle and space rental management')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'services'
+MODULE_ID = 'rentals'
+MODULE_NAME = _('Rental Management')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'key-outline'
+MODULE_DESCRIPTION = _('Equipment, vehicle and space rental management')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'services'
 
-    MENU = {
-        'label': _('Rental Management'),
-        'icon': 'key-outline',
-        'order': 36,
-    }
+MENU = {
+    'label': _('Rental Management'),
+    'icon': 'key-outline',
+    'order': 36,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Items'), 'icon': 'cube-outline', 'id': 'items'},
 {'label': _('Rentals'), 'icon': 'key-outline', 'id': 'rentals'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'rentals.view_rental',
+PERMISSIONS = [
+    'rentals.view_rental',
 'rentals.add_rental',
 'rentals.change_rental',
 'rentals.delete_rental',
@@ -32,4 +32,4 @@
 'rentals.add_rentalitem',
 'rentals.change_rentalitem',
 'rentals.manage_settings',
-    ]
+]
